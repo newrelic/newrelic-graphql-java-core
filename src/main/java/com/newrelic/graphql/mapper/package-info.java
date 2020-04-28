44cb24987aba0d957f758d3a0e947ea02a524e62
Jason R. Clark <jclark@newrelic.com>
2020-04-28 10:05:36 +0200

    Copyright notices

--- src/main/java/com/newrelic/graphql/mapper/package-info.java
+++ src/main/java/com/newrelic/graphql/mapper/package-info.java
@@ -1,2 +1,6 @@
+/*
+ * Copyright 2020 New Relic Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
 /** Mapping for GraphQL input types using Jackson */
 package com.newrelic.graphql.mapper;
--- src/main/java/com/newrelic/graphql/schema/scalars/package-info.java
+++ src/main/java/com/newrelic/graphql/schema/scalars/package-info.java
@@ -1,2 +1,6 @@
+/*
+ * Copyright 2020 New Relic Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
 /** Custom defined scalars and coercion logic */
 package com.newrelic.graphql.schema.scalars;
